5286cf48f7155e2f9c153f201d4905aa603a1881
Han Sheng <hanshengliang@outlook.com>
2022-02-07 15:15:16 +0800

    :zap: Config

---
+++ src/main/java/com/hansheng/springbootquickstart/student/StudentConfig.java
@@ -0,0 +1,20 @@
+package com.hansheng.springbootquickstart.student;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.LocalDate;
+import java.time.Month;
+
+@Configuration
+public class StudentConfig {
+
+    @Bean
+    CommandLineRunner commandLineRunner(StudentRepository studentRepository) {
+        return args -> {
+            studentRepository.save(new Student("Student 1", LocalDate.of(2000, Month.JANUARY, 1), 1, "student1@student1.com"));
+            studentRepository.save(new Student("Student 2", LocalDate.of(2000, Month.JANUARY, 1), 1, "student2@student2.com"));
+        };
+    }
+}
