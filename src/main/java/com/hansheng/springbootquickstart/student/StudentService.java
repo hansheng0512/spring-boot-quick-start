265f7c43c9cedf4309dcb5726780091e2484e143
Han Sheng <hanshengliang@outlook.com>
2022-02-07 15:07:27 +0800

    :zap: Repository

--- src/main/java/com/hansheng/springbootquickstart/student/StudentService.java
+++ src/main/java/com/hansheng/springbootquickstart/student/StudentService.java
@@ -1,6 +1,6 @@
 package com.hansheng.springbootquickstart.student;
 
-import org.springframework.stereotype.Component;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
@@ -9,9 +9,15 @@ import java.util.List;
 
 @Service
 public class StudentService {
+
+    private final StudentRepository studentRepository;
+
+    @Autowired
+    public StudentService(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
+
     public List<Student> getStudents() {
-        return List.of(
-                new Student(1L, "name", LocalDate.of(2000, Month.JANUARY, 1), 1, "mail@mail.com")
-        );
+        return studentRepository.findAll();
     }
 }
